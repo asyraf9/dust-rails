4a3c2e5361578f5d9f0ade0eb0ee1a5f17125ed6
Asyraf Abdul Rahman <asyraf9@gmail.com>
2012-09-05 10:25:06 +0800

    Update lib/dust-rails/rails/railtie.rb

--- lib/dust-rails/rails/railtie.rb
+++ lib/dust-rails/rails/railtie.rb
@@ -8,6 +8,7 @@ module Dust
       initializer "dust.configure" do |app|
         Dust.configure do |config|
           config.template_root = app.config.dust[:template_root] || 'app/assets/javascripts/templates/'
+          config.naming_convention = app.config.dust[:naming_convention] || 'template_root'
         end
       end
     end
